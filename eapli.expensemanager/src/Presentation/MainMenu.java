5375c551ea158a8bccbf6537c09abee1de705f37
i101132 <i101132@PCI028.dei.isep.ipp.pt>
2013-03-22 16:32:06 +0000

    Show Last Expense Use Case

--- eapli.expensemanager/src/Presentation/MainMenu.java
+++ eapli.expensemanager/src/Presentation/MainMenu.java
@@ -17,6 +17,7 @@ public class MainMenu {
         System.out.println("===================\n");
         
         System.out.println("1. Register an expense");
+        System.out.println("2. Show last expense");
         System.out.println("0. Exit\n\n");
         
         int option = Console.readInteger("Please choose a option");
@@ -27,6 +28,10 @@ public class MainMenu {
             case 1: 
                 ExpenseRegisterUI ui = new ExpenseRegisterUI();
                 ui.mainLoop();
+                break;
+            case 2:
+                ExpensesUI expui = new ExpensesUI();
+                expui.mainLoop();
                 break;
         }
     }
