0a8b2c8d1bb659a6301fd72fab303208fa6011e7
nbento <nbento@nbento-ASUS>
2013-03-26 01:21:41 +0000

    Add loop in menu

--- eapli.expensemanager/src/Presentation/MainMenu.java
+++ eapli.expensemanager/src/Presentation/MainMenu.java
@@ -13,32 +13,35 @@ import eapli.util.Console;
  */
 public class MainMenu {
     public void mainLoop() {
-        System.out.println("===================");
-        System.out.println("  EXPENSE MANAGER  ");
-        System.out.println("===================\n");
-        
-        System.out.println("1. Register an expense");
-        System.out.println("2. Add Expense Type");
-        System.out.println("3. Register an Expense with Expense Type");
-        System.out.println("0. Exit\n\n");
-        
-        int option = Console.readInteger("Please choose a option");
-        switch (option) {
-            case 0: 
-                System.out.println("bye bye ...");
-                return;
-            case 1: 
-                ExpenseRegisterUI ui = new ExpenseRegisterUI();
-                ui.mainLoop();
-                break;
-            case 2:
-                RegisterExpenseTypeUI uitype = new RegisterExpenseTypeUI();
-                uitype.mainLoop();
-                break;
-            case 3:
-                RegisterExpenseTypesUI uitypes = new RegisterExpenseTypesUI();
-                uitypes.mainLoop();
-                break;
-        }
+    int option;
+        do{
+            System.out.println("===================");
+            System.out.println("  EXPENSE MANAGER  ");
+            System.out.println("===================\n");
+
+            System.out.println("1. Register an expense");
+            System.out.println("2. Add Expense Type");
+            System.out.println("3. Register an Expense with Expense Type");
+            System.out.println("0. Exit\n\n");
+
+            option = Console.readInteger("Please choose a option");
+            switch (option) {
+                case 0: 
+                    System.out.println("bye bye ...");
+                    return;
+                case 1: 
+                    ExpenseRegisterUI ui = new ExpenseRegisterUI();
+                    ui.mainLoop();
+                    break;
+                case 2:
+                    RegisterExpenseTypeUI uitype = new RegisterExpenseTypeUI();
+                    uitype.mainLoop();
+                    break;
+                case 3:
+                    RegisterExpenseTypesUI uitypes = new RegisterExpenseTypesUI();
+                    uitypes.mainLoop();
+                    break;
+            }
+        }while(option!=0);
     }
 }
