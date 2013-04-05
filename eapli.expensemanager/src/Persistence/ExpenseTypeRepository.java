8a26b0e5fea3564bd1386f9755ec07058525ad5c
nbento <nbento@nbento-ASUS>
2013-04-05 17:52:37 +0100

    Adição do metodo getAllExpenseType()

--- eapli.expensemanager/src/Persistence/ExpenseTypeRepository.java
+++ eapli.expensemanager/src/Persistence/ExpenseTypeRepository.java
@@ -34,5 +34,9 @@ public class ExpenseTypeRepository implements IExpenseTypeRepository {
       return listExpenseType;
     }
     
+    public List<ExpenseType> getExpenseType(){
+        
+        return listExpenseType;
+    }
     
 }
