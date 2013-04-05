96569dfac03cf52794e05b8a09f6b68b386c84e4
nbento <nbento@nbento-HOME>
2013-04-05 23:00:18 +0100

    Repositorio Cartões de Debito

---
+++ eapli.expensemanager/src/Persistence/DebiteCardRepository.java
@@ -0,0 +1,40 @@
+
+package Persistence;
+
+import Model.DebitCard;
+import Model.ExpenseType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe repositorio para armazenar os cartões de debito
+ * 
+ * @author nbento
+ */
+public class DebiteCardRepository {
+
+    private static ArrayList<DebitCard> listDebitCards = new ArrayList<DebitCard>();
+    
+    private DebiteCardRepository(){}
+    
+    /**
+     * Adicionar uma novo cartão de debito no repositorio
+     * 
+     * @param dc - Objeto cartão de debito
+     */
+    public void save(DebitCard dc){
+        if (dc==null) throw new IllegalArgumentException();
+        listDebitCards.add(dc);
+    }
+    
+    /**
+     * Devolve todos os cartões de debito do repositorio
+     * 
+     * @return Lista completa dos cartões de debito
+     */
+    public List<DebitCard> getAllDebitCard(){
+        return listDebitCards;
+    }
+    
+    
+}
