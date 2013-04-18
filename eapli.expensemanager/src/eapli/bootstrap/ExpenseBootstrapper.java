7893e93cae37beaf5b685961efa386fb05b9219c
nbento <nbento@nbento-ASUS>
2013-04-18 14:27:18 +0100

    Correcção de Erros: Adição de description nas classes paymentMeanController e ExpenseBootstrapper

--- eapli.expensemanager/src/eapli/bootstrap/ExpenseBootstrapper.java
+++ eapli.expensemanager/src/eapli/bootstrap/ExpenseBootstrapper.java
@@ -18,15 +18,15 @@ public class ExpenseBootstrapper {
 
     static {
         ExpenseRepository repo = new ExpenseRepository();
-        repo.save(new Expense("Autocarro", 2013, 4, 9, new BigDecimal(2.4), new PaymentMean("money") ));
-        repo.save(new Expense("Almoço", 2013, 3, 5, new BigDecimal(12.4),new PaymentMean("money")));
-        repo.save(new Expense("Sapatilhas", 2013, 3, 4, new BigDecimal(123.4), new PaymentMean("money")));
-        repo.save(new Expense("Cinema", 2013, 3, 4, new BigDecimal(5.0), new PaymentMean("money")));
+        repo.save(new Expense("Autocarro", 2013, 4, 9, new BigDecimal(2.4), new PaymentMean("Teste Autocarro", "money") ));
+        repo.save(new Expense("Almoço", 2013, 3, 5, new BigDecimal(12.4),new PaymentMean("Teste Almoço","money")));
+        repo.save(new Expense("Sapatilhas", 2013, 3, 4, new BigDecimal(123.4), new PaymentMean("Teste Sapatilhas","money")));
+        repo.save(new Expense("Cinema", 2013, 3, 4, new BigDecimal(5.0), new PaymentMean("Teste Cinema","money")));
     
         PaymentMeanRepository repo2=new PaymentMeanRepository();
-        repo2.save(new PaymentMean("Cartao Debito"));
-        repo2.save(new PaymentMean("Cartao Credito"));
-        repo2.save(new PaymentMean("Dinheiro"));
+        repo2.save(new PaymentMean("Teste Cartao Debito","Cartao Debito"));
+        repo2.save(new PaymentMean("Teste Cartao Credito","Cartao Credito"));
+        repo2.save(new PaymentMean("Teste Dinheiro","Dinheiro"));
     
     }
 }
