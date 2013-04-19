a0e023c2d9e0d962de7b1a056c0d85e13c8f554b
nbentoneves <nbento.neves@gmail.com>
2013-04-19 17:29:19 +0100

    #Correcção de Erros
    ####
    #Add:---
    #Mod:Alteração do construtor de uma despesa, formatação de código  
    #Fix:---  
    #Rem:---   
    ####
    
    @autor: nbentoneves

--- eapli.expensemanager/src/eapli/bootstrap/ExpenseBootstrapper.java
+++ eapli.expensemanager/src/eapli/bootstrap/ExpenseBootstrapper.java
@@ -11,6 +11,7 @@ import Persistence.ExpenseTypeRepository;
 import Model.ExpenseType;
 import Persistence.PaymentMeanRepository;
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  *
@@ -20,19 +21,19 @@ public class ExpenseBootstrapper {
 
     static {
         ExpenseRepository repo = new ExpenseRepository();
-        repo.save(new Expense("Autocarro", 2013, 4, 9, new BigDecimal(2.4), new PaymentMean("Teste Autocarro", "money") ));
-        repo.save(new Expense("Almoço", 2013, 3, 5, new BigDecimal(12.4),new PaymentMean("Teste Almoço","money")));
-        repo.save(new Expense("Sapatilhas", 2013, 3, 4, new BigDecimal(123.4), new PaymentMean("Teste Sapatilhas","money")));
-        repo.save(new Expense("Cinema", 2013, 3, 4, new BigDecimal(5.0), new PaymentMean("Teste Cinema","money")));
-    
-        PaymentMeanRepository repo2=new PaymentMeanRepository();
-        repo2.save(new PaymentMean("Teste Cartao Debito","Cartao Debito"));
-        repo2.save(new PaymentMean("Teste Cartao Credito","Cartao Credito"));
-        repo2.save(new PaymentMean("Teste Dinheiro","Dinheiro"));
-        
-        ExpenseTypeRepository repo3=new ExpenseTypeRepository();
+        repo.save(new Expense("Autocarro", new Date(2013, 4, 9), new BigDecimal(2.4), new ExpenseType("Despesas de transportes", "Transportes"), new PaymentMean("Teste Autocarro", "money")));
+        repo.save(new Expense("Almoço", new Date(2013, 3, 5), new BigDecimal(12.4), new ExpenseType("Despesas de alimentação", "Alimentação"), new PaymentMean("Teste Almoço", "money")));
+        repo.save(new Expense("Sapatilhas", new Date(2013, 3, 4), new BigDecimal(123.4), new ExpenseType("Despesas de vestuario", "Vestuario"), new PaymentMean("Teste Sapatilhas", "money")));
+        repo.save(new Expense("Cinema", new Date(2013, 3, 4), new BigDecimal(5.0), new ExpenseType("Despesas de lazer", "Lazer"), new PaymentMean("Teste Cinema", "money")));
+
+        PaymentMeanRepository repo2 = new PaymentMeanRepository();
+        repo2.save(new PaymentMean("Teste Cartao Debito", "Cartao Debito"));
+        repo2.save(new PaymentMean("Teste Cartao Credito", "Cartao Credito"));
+        repo2.save(new PaymentMean("Teste Dinheiro", "Dinheiro"));
+
+        ExpenseTypeRepository repo3 = new ExpenseTypeRepository();
         repo3.save(new ExpenseType("Despesas de alimentação", "Alimentação"));
         repo3.save(new ExpenseType("Despesas de transportes", "Transportes"));
-    
+
     }
 }
