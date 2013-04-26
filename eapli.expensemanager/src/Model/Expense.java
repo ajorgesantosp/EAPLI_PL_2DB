73a0f7030aff5aa076974e0017bf8b8fe87af3d2
nbentoneves <nbento.neves@gmail.com>
2013-04-26 22:35:40 +0100

    ##Metodo Equals
    ####
    #Add:Criação do metodo equals para comparação de obj
    #Mod:Formatação do código para uma melhor visualização  
    #Fix:---  
    #Rem:---   
    ####
    
    @autor: nbentoneves
    
    ####
    #Add:---
    #Mod:Formatação do código para uma melhor visualização  
    #Fix:---  
    #Rem:---   
    ####
    
    @autor: nbentoneves

--- eapli.expensemanager/src/Model/Expense.java
+++ eapli.expensemanager/src/Model/Expense.java
@@ -4,8 +4,9 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 /**
- *
- * @author nbento
+ * Classe de despesas
+ * 
+ * @autor 1110186 & 1110590
  */
 public class Expense extends BaseType {
 
@@ -56,5 +57,35 @@ public class Expense extends BaseType {
         System.out.println("Valor: " + this.getAmount().doubleValue());
         System.out.println("**********************************\n");
 
+    }
+    
+    /**
+     * Comparação de objectos
+     * 
+     * @autor 1110186 & 1110590
+     * @param other - Objecto a ser comparado
+     * @return True -> Objectos iguais | False -> Objectos diferentes
+     */
+    @Override
+    public boolean equals(Object other){
+        boolean result = false;
+        
+        if(other instanceof Expense){
+            Expense that = (Expense) other;
+            result = (this.id == that.id && this.description.equalsIgnoreCase(that.description) && this.d.equals(that.d) && 
+                    this.amount == that.amount && this.mean.equals(that.mean) && this.type.equals(that.type));
+        }
+        
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + (this.amount != null ? this.amount.hashCode() : 0);
+        hash = 79 * hash + (this.type != null ? this.type.hashCode() : 0);
+        hash = 79 * hash + (this.mean != null ? this.mean.hashCode() : 0);
+        hash = 79 * hash + (this.d != null ? this.d.hashCode() : 0);
+        return hash;
     }
 }
