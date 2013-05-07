6873b0ebd721f4c73cf559c4dd60bff645307dd6
nbentoneves <nbento.neves@gmail.com>
2013-05-07 22:25:22 +0100

    #Adição Metodos na Classe
    ####
    #Add:Adição de metodo de comparação na classe
    #Mod:Formatação do código para uma melhor visualização, correcção do nome da classe
    #Fix:---  
    #Rem:---
    ####
    
    @autor: nbentoneves

--- eapli.expensemanager/src/Model/IncomeType.java
+++ eapli.expensemanager/src/Model/IncomeType.java
@@ -17,5 +17,33 @@ public class IncomeType {
         this.description = description;
     }
     
+        /**
+     * Comparação de objectos
+     *
+     * @autor 1110186 & 1110590
+     * @param other - Objecto a comparar
+     * @return True -> Objectos iguais | False -> Objectos diferentes
+     */
+    @Override
+    public boolean equals(Object other) {
+        boolean result = false;
+
+        if (other instanceof IncomeType) {
+            IncomeType that = (IncomeType) other;
+            result = (this.description.equalsIgnoreCase(that.description)
+                    && this.type.equalsIgnoreCase(that.type));
+        }
+
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + (this.type != null ? this.type.hashCode() : 0);
+        hash = 59 * hash + (this.description != null ? this.description.hashCode() : 0);
+        return hash;
+    }
+    
 }
 
