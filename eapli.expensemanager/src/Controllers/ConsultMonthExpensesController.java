912fa55bfd4347c76f304a7581fbff7861e01522
Mafalda Landeiro <1110590@isep.ipp.pt>
2013-05-08 22:01:39 +0100

    Correção de erros

--- eapli.expensemanager/src/Controllers/ConsultMonthExpensesController.java
+++ eapli.expensemanager/src/Controllers/ConsultMonthExpensesController.java
@@ -8,26 +8,19 @@ package Controllers;
  */
 
 import Model.Expense;
-import Persistence.inMemory.ExpenseRepository;
-import eapli.exception.EmptyList;
+import Model.RecordExpense;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class ConsultMonthExpensesController {
     
     public List<Expense> getMonthExpenses(int MonthNumber,int YearNumber){
-        ExpenseRepository expRepo=new ExpenseRepository();
+        RecordExpense expRepo=new RecordExpense();
         
-        List<Expense> expenses = null;
+        List<Expense> expenses=expRepo.getListExpenditures();
         List<Expense> aux;
         aux = new ArrayList<Expense>();
-        try {
-            expenses=expRepo.getList();
-        } catch (EmptyList ex) {
-            Logger.getLogger(ConsultMonthExpensesController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
         for(int i=0;i<expenses.size();i++){
             if((expenses.get(i).getDate().getMonth()==MonthNumber) && (expenses.get(i).getDate().getYear()==YearNumber)){
                 aux.add(expenses.get(i));
