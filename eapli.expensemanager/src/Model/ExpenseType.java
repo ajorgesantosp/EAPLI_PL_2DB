a812903a5e470d177deee3d107bae49cf8d07279
Mafalda Landeiro <1110590@isep.ipp.pt>
2013-05-15 19:54:12 +0100

    Adição do compare() na classe

--- eapli.expensemanager/src/Model/ExpenseType.java
+++ eapli.expensemanager/src/Model/ExpenseType.java
@@ -6,7 +6,7 @@ package Model;
 
 import javax.persistence.Entity;
 import javax.persistence.Entity;
-
+import java.util.Comparator;
 /**
  *
  * @author dg5s5dfsd5fs5d
@@ -70,4 +70,22 @@ public class ExpenseType extends BaseType {
         hash = 67 * hash + (this.type != null ? this.type.hashCode() : 0);
         return hash;
     }
+
+     /**
+     * Comparator
+     *
+     * @autor 1110186 & 1110590
+     * @param ExpenseType et1 e ExpenseType et2
+     * @return a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object
+     */
+    
+    public static Comparator<ExpenseType> ExpenseTypeNameComparator=new Comparator<ExpenseType>(){
+        @Override
+        public int compare(ExpenseType et1,ExpenseType et2){
+            String typeName1=et1.getType().toUpperCase();
+            String typeName2=et2.getType().toUpperCase();
+            
+            return typeName1.compareTo(typeName2);
+        }
+    };
 }
