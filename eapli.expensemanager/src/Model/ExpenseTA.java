c9af790d746af9112b8c4ca24b83b35d04ce05f8
i100905 <i100905@PCI029.dei.isep.ipp.pt>
2013-05-16 15:02:31 +0100

    list expenditures

---
+++ eapli.expensemanager/src/Model/ExpenseTA.java
@@ -0,0 +1,61 @@
+package Model;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import java.util.Comparator;
+/**
+ *
+ * @author 1100905  & 1070413
+ */
+
+public class ExpenseTA{
+
+    
+    private ExpenseType type;
+    private BigDecimal amount;
+
+    protected ExpenseTA() {
+    }
+
+    public ExpenseTA(ExpenseType type, BigDecimal amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    /**
+     * @return the type
+     */
+    public ExpenseType getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(ExpenseType type) {
+        this.type = type;
+    }
+    
+    /**
+     * @return the amount
+     */
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense Type-Amount:\nType= " + type + "\namount= " + amount + '\n';
+    }
+
+  
+
+   
+}
